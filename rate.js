c6d4af68ae585f21223020bc8bdf440767ef4979
arifmamon <arifmamon222@gmail.com>
2022-01-09 13:47:07 +0530

    main

---
+++ rate.js
@@ -0,0 +1,22 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const loop = 2000;
+  for (let i = 0; i < loop; i++) {
+  const browser = await puppeteer.launch();
+
+  const page = await browser.newPage();
+
+  const url = 'https://readmanganato.com/manga-ao977497/';
+  await page.goto(url);
+  await page.waitForTimeout(2000);
+  await page.click('.rate_star');
+  await page.screenshot({ path: 'example.png' ,fullPage: true});
+  const name = await page.$eval('.story-info-right > h1 ', element => element.textContent);
+  await page.waitForTimeout(1000);
+  
+  
+  await browser.close();
+  console.log( name ,"Rated " , i , "Times");
+  }
+})();
