b01daa99533951e2c02b0d660fa5dc2a1f1bd143
arifmamon <arifmamon222@gmail.com>
2022-02-18 12:28:40 +0530

    Update rate.js

--- rate.js
+++ rate.js
@@ -12,7 +12,7 @@ const puppeteer = require('puppeteer');
 
   const page = await browser.newPage();
   const kanojo = 'https://readmanganato.com/manga-ao977497/'
-  const url = 'https://readmanganato.com/manga-mh989864';
+  const url = 'https://readmanganato.com/manga-ao977497';
   await page.goto(url,{waitUntil: 'load', timeout: 0});
   await page.waitForTimeout(2000);
   await page.click('.rate_row');
