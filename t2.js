6bd10574a5f535e14f7ce1428c53da64b0e76370
arifmamon <arifmamon222@gmail.com>
2022-03-24 08:12:09 +0530

    added toon

--- t2.js
+++ t2.js
@@ -7,18 +7,28 @@ const puppeteer = require('puppeteer');
     args: [
         '--no-sandbox',
         '--disable-setuid-sandbox',
-        'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
+        'user-agent=Mozilla/5.0 (Freebox; Linux i686) AppleWebKit/538.1 (KHTML, like Gecko) Navigateur web/1.0 Version/7.0 Safari/538.1 [ip:83.159.244.129]',
     ]
 });
 
+  
   const page = await browser.newPage();
     // 15 people rated
   const url = 'https://toonily.com/webtoon/tonight-youre-my-dinner/';
   await page.goto(url,{waitUntil: 'load', timeout: 0});
-  await page.waitForTimeout(10000);
- // await page.click('.btn-adult-confirm');
+
+  await page.waitForTimeout(2000);
+  // 484
+  await page.waitForSelector('.ratings_stars');
+
+
+
+
+  await page.click('.ratings_stars');
+
+  await page.waitForTimeout(5000);
  // await page.click('.ratings_stars');
- await page.screenshot({ path: 'example.png' ,fullPage: true});
+ await page.screenshot({ path: 'toon.png' ,fullPage: true});
   //const name = await page.$eval('.post-title > h1 ', element => element.textContent);
   await page.waitForTimeout(1000);
  // await page.screenshot({ path: 'rate.png' ,fullPage: true});
